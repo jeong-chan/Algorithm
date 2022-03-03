e3f6502f3332186ccd0e3b747d14db95cec12614
jeong-chan <77624879+jeong-chan@users.noreply.github.com>
2022-03-04 00:24:15 +0900

    Update BJ_1065_다이얼.java

--- Baekjoon_java/BJ_1065_다이얼.java
+++ Baekjoon_java/BJ_1065_다이얼.java
@@ -1,3 +1,5 @@
+package ssafy.com.obj.basic;
+
 import java.util.*;
 
 public class Main {
@@ -9,9 +11,9 @@ public class Main {
 		String str_tmp = scanner.nextLine();
 		char a = 'A';
 		int index = 0;
-		int[] time = new int[10];
-		char[][] dial = new char[10][4];
-		for (int i = 0; i < 10; i++) {
+		int[] time = new int[10]; //각 숫자마다 걸리는 시간을 저장할 배열
+		char[][] dial = new char[10][4]; //0~9 각 번호에 해당하는 문자를 저장할 2차원 배열 생성
+		for (int i = 0; i < 10; i++) { //반복을 돌며 각 숫자에 해당하는 문자를 전부 저장
 			if (i == 7) {
 				for (int j = 1; j < 5; j++) {
 					dial[i][j - 1] = (char) (a + j - 1);
@@ -33,6 +35,8 @@ public class Main {
 					dial[i][j] = a++;
 				}
 			}
+
+			//각 버튼마다 걸리는 시간 저장
 			if(i==1) {
 				time[1]=2;
 			}else if(i==0){
@@ -42,10 +46,13 @@ public class Main {
 			}
 		}
 
+		//문자열의 길이만큼 반복
 		for (int k = 0; k < str_tmp.length(); k++) {
+			//알파벳을 통해 해당 알파벳이 들어있는 숫자 탐색
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 4; j++) {
 					if(dial[i][j] == str_tmp.charAt(k)) {
+						//숫자를 찾으면 해당하는 숫자가 걸리는 시간 누적 합
 						index += time[i];
 					}
 					//System.out.print(dial[i][j]);
@@ -53,6 +60,7 @@ public class Main {
 				//	System.out.println(i);
 			}
 		}
+		//누적합 프린트
 		System.out.println(index);
 		scanner.close();
 	}
