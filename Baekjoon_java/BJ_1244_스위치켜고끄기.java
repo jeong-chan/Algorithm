e0d094a901c03ea5ddc515f312a2d61225280e5d
jeong-chan <77624879+jeong-chan@users.noreply.github.com>
2022-03-19 18:52:37 +0900

    Update BJ_1244_스위치켜고끄기.java

--- Baekjoon_java/BJ_1244_스위치켜고끄기.java
+++ Baekjoon_java/BJ_1244_스위치켜고끄기.java
@@ -5,86 +5,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+
 public class Main {
+	static int[] swithes;    
+
+  //남학생 동작 메서드
+	public static void boy_student(int a) {
+
+		for(int i=1; i*a<=swithes.length; i++) {
+			if(swithes[i*a-1]==1)
+				swithes[i*a-1]=0;
+			else
+				swithes[i*a-1]=1;
+		}
+	}
+
+  //여학생 동작 메서드
+	public static void girl_student(int a) {
+		int right = a-1;
+		int left = a-1;
+
+		while(true){
+			right++;
+			left--;
+			if(right>=swithes.length || left<0 || swithes[left]!=swithes[right]) {
+				right--;
+				left++;
+				break;
+			}
+		}
+
+		for(int i=left; i<=right; i++) {
+			if(swithes[i]==1) {
+				swithes[i]=0;
+			}
+			else {
+				swithes[i]=1;
+			}
+		}
+	}
+  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int num_switch = sc.nextInt();
-		String switches[] = new String[num_switch];
+		int N = sc.nextInt();
+		swithes = new int[N];
 		sc.nextLine();
-		switches = sc.nextLine().split(" ");
-		/*for(int i =0; i<switches.length;i++) {
-		System.out.printf(switches[i]);
-		}*/
+		String[] on_off = sc.nextLine().split(" ");
 
-		int num_student = sc.nextInt();
-		int gender[] = new int[num_student];
-		int getnumber[] = new int[num_student];
-
-		for(int i =0; i<num_student;i++) {
-			gender[i] = sc.nextInt();
-			getnumber[i] = sc.nextInt();
+		for(int i=0; i<N; i++) {
+			swithes[i] = Integer.parseInt(on_off[i]);
 		}
 
-		for(int i=0; i<num_student;i++) {
-			int tmp_gender = gender[i];
-			int tmp_getnumber = getnumber[i];
-			if(tmp_gender==1) {
-
-				for(int k = 1; k<=100; k++) {
-					if(tmp_getnumber*k-1>=switches.length) {
-						break;
-					}
-					if(switches[tmp_getnumber*k-1].equals("0")) {
-						switches[tmp_getnumber*k-1] = "1";
-					}else {
-						switches[tmp_getnumber*k-1] = "0";
-					}
-				}
-			}else {
-				for(int k=0; k<100; k++) {
-					if(tmp_getnumber-1-k<0||tmp_getnumber-1+k>=switches.length || !switches[tmp_getnumber-1-k].equals(switches[tmp_getnumber-1+k])) {
-						break;
-					}
-
-					if(switches[tmp_getnumber-1-k].equals(switches[tmp_getnumber-1+k])) {
-						
-							if(switches[tmp_getnumber-1-k].equals("1") ) {
-								switches[tmp_getnumber-1-k] ="0";
-								switches[tmp_getnumber-1+k] ="0";
-							}else {
-								switches[tmp_getnumber-1-k] ="1";
-								switches[tmp_getnumber-1+k] ="1";
-							}
-
-							continue;
-						
-					}else {
-						break;
-						
-					}
-				}
-				if(switches[tmp_getnumber-1].equals("1")) {
-					switches[tmp_getnumber-1]="0";
-				}else {
-					switches[tmp_getnumber-1]="1";
-				}
+		int M = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i=0; i<M; i++) {
+			String[]	student = sc.nextLine().split(" ");
+			if(Integer.parseInt(student[0])==1) {
+				boy_student(Integer.parseInt(student[1]));
 			}
-			for(int o =0; o<switches.length;o++) {
-				System.out.printf(switches[o]);
-				}
-			System.out.println();
-
+			else {
+				girl_student(Integer.parseInt(student[1]));
+			}
 		}
-		/*for(int i =0; i<switches.length; i++) {
-			if(i != 0 && i%20 ==0) {
-				System.out.print("\n");
+		
+
+    //한줄에 20개씩 
+		for(int i=0; i<N; i++) {
+			if(i!=0 && i%20==0) {
+				System.out.println();
 			}
-			if(i ==0) {
-				System.out.print(switches[0]);
-			}else
-			System.out.print(" "+switches[i]);
-		}*/
+			System.out.print(swithes[i]+" ");
+		}
 	}
+
+
 }
