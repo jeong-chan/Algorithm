dcfcd8268105f07e2b63c89f705ee3d1cb6be05b
정찬우 <77624879+jeong-chan@users.noreply.github.com>
2022-07-17 21:42:41 +0900

    Create BJ_9295_주사위.java

---
+++ Baekjoon_java/BJ_9295_주사위.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+
+            sb.append("Case ").append(i).append(": ").append(first+second).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
